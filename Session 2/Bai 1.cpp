0b7c3e495f81cff3966e64e52248df9e41afa272
phucdt <dotienphuc1607@gmail.com>
2025-10-07 19:30:33 +0700

    Update Bai 1.cpp

--- Session 2/Bai 1.cpp
+++ Session 2/Bai 1.cpp
@@ -1,13 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-	short a = 10 // short: so nguyen nho, 2 byte
-int b = 5 // int: so thuc 
-long 
-long long
-unsigned short
-unsigned int
-unsigned long
-unsigned long long
-char
+int main(){
+    short a = 1; // short: so nguyen nho, 2 byte
+    int b = 20; // int: so nguyen thuong, 4 byte
+    long c = 3000; // long: so nguyen lon, 4 hoac 8 byte
+    long long d = 40000; // long long: so nguyen rat lon, 8 byte
+    unsigned short e = 40; // unsigned short: so nguyen nho khong dau, chi luu gia tri duong (0 den 65535)
+    unsigned int f = 500;    // unsigned int: so nguyen khong dau, chi luu gia tri duong (0 den 4 ty)
+    unsigned long g = 6000; // unsigned long: so nguyen lon khong dau, dung khi luu lon hon unsigned int
+    unsigned long long h = 70000;    // unsigned long long: so nguyen rat lon khong dau, luu gia tri rat lon
+    char i = 'A';    // char: ky tu, chiem 1 byte
+
+	
+    return 0;
 }
