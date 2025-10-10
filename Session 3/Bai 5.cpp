ebeebcae1845ad1d4d347163cfbfa81f5e97280d
phucdt <dotienphuc1607@gmail.com>
2025-10-10 09:48:17 +0700

    Add files via upload

---
+++ Session 3/Bai 5.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    printf("\t \t \t DANH SACH SINH VIEN\n\n");
+    printf("STT \t Ho va ten \t \t Tuoi \t So dien thoai\t Email \n ");
+    printf("---------------------------------------------------------------\n");
+    printf("1 \t Nguyen Van A \t \t 21 \t 0904488481 \t anv@rikkeiacademy.com \n");
+    
+    printf("2 \t Nguyen Van B \t \t 21 \t 0904488481 \t bnv@rikkeiacademy.com \n");
+    
+    printf("3 \t Nguyen Van C \t \t 21 \t 0904488481 \t cnv@rikkeiacademy.com \n");
+    
+    printf("4 \t Nguyen Van D \t \t 21 \t 0904488481 \t dnv@rikkeiacademy.com \n");
+    
+    printf("5 \t Nguyen Van E \t \t 21 \t 0904488481 \t env@rikkeiacademy.com \n");
+    
+    printf("6 \t Nguyen Van F \t \t 21 \t 0904488481 \t fnv@rikkeiacademy.com \n");
+    
+    printf("7 \t Nguyen Van G \t \t 21 \t 0904488481 \t gnv@rikkeiacademy.com \n");
+    
+    
+    printf("9 \t Nguyen Van I \t \t 21 \t 0904488481 \t inv@rikkeiacademy.com \n");
+    
+    printf("10 \t Nguyen Van K \t \t 21 \t 0904488481 \t knv@rikkeiacademy.com \n");
+
+    return 0;
+}
